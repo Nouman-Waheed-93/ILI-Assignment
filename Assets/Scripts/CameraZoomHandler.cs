da3d7d8add672d1c2203eb6a5127dfe598588c93
Nouman Waheed <nouman1993@live.com>
2021-02-04 21:03:27 +0500

    Before Quaternion Multiplications

---
+++ Assets/Scripts/CameraZoomHandler.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace FireTruckStoreApp
+{
+    public class CameraZoomHandler : MonoBehaviour
+    {
+        [SerializeField]
+        Camera camera;
+        [SerializeField]
+        float zoomStep;
+        [SerializeField]
+        float minFOV;
+        [SerializeField]
+        float maxFOV;
+
+        public void ZoomIn()
+        {
+            float FOV = camera.fieldOfView;
+            FOV -= zoomStep;
+            if (FOV < minFOV)
+                FOV = minFOV;
+            camera.fieldOfView = FOV;
+        }
+
+        public void ZoomOut()
+        {
+            float FOV = camera.fieldOfView;
+            FOV += zoomStep;
+            if (FOV > maxFOV)
+                FOV = maxFOV;
+            camera.fieldOfView = FOV;
+        }
+    }
+}
